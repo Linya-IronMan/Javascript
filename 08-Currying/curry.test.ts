6d33694f5ce7725fe4af1dbbacf217987c0b952e
林崖 <cdthanye32@163.com>
2024-02-25 20:27:24 +0800

    curry

---
+++ 08-Currying/curry.test.ts
@@ -0,0 +1,13 @@
+import { curry } from './curry';
+
+describe('curry', () => {
+    it('add', () => {
+        function add(a: number, b: number, c: number) {
+            return a + b + c;
+        }
+        const res1 = add(10, 20, 30);
+        const curryAdd = curry(add);
+        const res2 = curryAdd(10)(20)(30);
+        expect(res1).toBe(res2);
+    });
+});
---
+++ 08-Currying/curry.ts
@@ -0,0 +1,26 @@
+export const curry = (fn: Function) => {
+    const fnArgsLength = fn.length; // 传入函数的参数长度
+    let args: any[] = [];
+
+    // NOTE: TS 中独立的函数，this 需要生命类型
+    function calc(this: any, ...newArgs: any[]) {
+        // 积累参数
+        args = [...args, ...newArgs];
+        if (args.length < fnArgsLength) {
+            // 参数不够
+            return calc;
+        } else {
+            // 参数足够
+            return fn.apply(this, args.slice(0, fnArgsLength));
+        }
+    }
+    return calc;
+};
+
+function add(a: number, b: number, c: number): number {
+    return a + b + c;
+}
+
+const curryAdd = curry(add);
+const res = curryAdd(10)(20)(30);
+console.info('curry add', res);
